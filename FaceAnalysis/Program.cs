089ffbb35c0a6276a5c9e7d2482062eb79d0b309
Angelo <chise_elunico@hotmail.com>
2023-12-20 15:05:42 -0500

    feath: Finished video reader

--- FaceAnalysis/Program.cs
+++ FaceAnalysis/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        string videoPath = "C:\\Users\\SleepingF\\Desktop\\test10.mp4";
+        string outputDirectory = "C:\\Users\\SleepingF\\Desktop\\saveFrames";
+
+      
+
+
+
+        VideoCapture videoCapture = new VideoCapture(videoPath);
+
+        if (!videoCapture.IsOpened)
+        {
+            Console.WriteLine("Error");
+            return;
+        }
+
+        System.IO.Directory.CreateDirectory(outputDirectory);
+
+       
+
+
+
+        int frameCount = (int)videoCapture.Get(CapProp.FrameCount);
+        for(int i = 0; i< frameCount; i++)
+        {
+            Mat frame= new Mat();
+            videoCapture.Read(frame);
+
+            string outputPath = System.IO.Path.Combine(outputDirectory, $"frame_{i}.jpg");
+            CvInvoke.Imwrite(outputPath, frame);
+
+            
+        }
+        Console.WriteLine($"Cantidad total de frames en el video: {frameCount}");
+        Console.WriteLine("Frames guardados con éxito, Angelo.");
+    }
+}
+
+
